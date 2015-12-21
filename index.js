a94196ae7890e4935bf0876705fd232f3810e9f7
henryleu <henryleu@126.com>
2015-12-21 19:17:16 +0800

    update wechat

--- index.js
+++ index.js
@@ -35,12 +35,12 @@ var settings = module.exports = {
   },
 
   wechat: {
-    appKey: 'wx23f1709f7727051f',
-    appSecret: '977f6080e128d465b673deb79e3d31b8',
+    appKey: 'wx8ecd17a4beabe76a',
+    appSecret: '74ad148c79ac88f844ad98b72d59de96',
     token: 'trillers',
     encodingAESKey: '9zYRktc6N1WPyqH6hXq38tJC2CVDaLjHIkxRpihzmx3',
-    siteId: 'gh_afc333104d2a',
-    siteName: '错题本'
+    siteId: 'gh_76ea74206101',
+    siteName: '跟谁学西安'
   },
 
   platform: {
